2c84b077b900876ac1de46b9fc370b93dff95978
Naveen Dwivedi <75691459+naveen-dwivedi-7@users.noreply.github.com>
2025-09-18 21:25:03 +0530

    implemented front end

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -1,41 +1,114 @@
+const moongoose = require('mongoose');
 
 const categoryModel = require('../models/Category');
 const newsModel = require('../models/News');
 const userModel = require('../models/User');
+const settingModel = require('../models/Setting');
 const commentModel = require('../models/Comment');
+const paginate = require('../utils/paginate')
+const createError = require('../utils/error-message')
 
-const index = async (req, res) => {
-       res.render('index')
-   
+const index = async (req,res) => {
+
+  const paginatedNews = await paginate(newsModel, {}, 
+                                      req.query, {
+                                      populate: [
+                                        { path: 'category', select: 'name slug' },
+                                        { path: 'author', select: 'fullname' }
+                                      ],   
+                                      sort: '-createdAt' })
+
+  // res.json({ paginatedNews })
+  res.render('index', { paginatedNews , query: req.query})
+ }
+
+
+const articleByCategories = async (req,res, next) => {
+  const category = await categoryModel.findOne({ slug: req.params.name });
+  if (!category) {
+    return next(createError('Category not found', 404));
+  }
+  const paginatedNews = await paginate(newsModel, { category: category._id }, 
+                                      req.query, {
+                                      populate: [
+                                        { path: 'category', select: 'name slug' },
+                                        { path: 'author', select: 'fullname' }
+                                      ],   
+                                      sort: '-createdAt' })
+
+  res.render('category', { paginatedNews, category, query: req.query })
+ }
+
+const singleArticle = async (req,res, next) => { 
+  const singleNews = await newsModel.findById(req.params.id)
+                        .populate('category',{'name':1, 'slug':1})
+                        .populate('author','fullname')
+                        .sort({createdAt: -1})
+
+  if(!singleNews) return next(createError('Article not found', 404));                     
+
+  // Get all comments for this article
+  const comments = await commentModel.find({ article: req.params.id, status: 'approved' })
+  .sort('-createdAt')                     
+
+  // res.json({ singleNews, comments })                                     
+  res.render('single', { singleNews, comments })
 }
 
+const search = async (req,res) => {
+  const searchQuery = req.query.search
 
-const articleByCategories = async (req, res, next) => {
-  res.render('category')
+  const paginatedNews = await paginate(newsModel, {
+                              $or: [
+                                { title: { $regex: searchQuery, $options: 'i' } },
+                                { content: { $regex: searchQuery, $options: 'i' } }
+                              ]
+                            }, 
+                            req.query, {
+                            populate: [
+                              { path: 'category', select: 'name slug' },
+                              { path: 'author', select: 'fullname' }
+                            ],   
+                            sort: '-createdAt' })
+
+
+  res.render('search', { paginatedNews, searchQuery, query: req.query })
+ }
+
+const author = async (req,res, next) => { 
+  const author = await userModel.findOne({ _id: req.params.name });
+  if (!author) {
+    return next(createError('Author not found', 404));
+  }
+
+  const paginatedNews = await paginate(newsModel, { author: req.params.name  }, 
+                                      req.query, {
+                                      populate: [
+                                        { path: 'category', select: 'name slug' },
+                                        { path: 'author', select: 'fullname' }
+                                      ],   
+                                      sort: '-createdAt' })
+
+
+  res.render('author', { paginatedNews, author, query: req.query })
 }
 
-const singleArticle = async (req, res, next) => {
-    res.render('single')
-}
-
-const search = async (req, res) => {
-   res.render('search')
-}
-
-const author = async (req, res, next) => {
-   res.render('author')
-}
-
-const addComment = async (req, res, next) => {
-    
-    
+const addComment = async (req,res, next) => { 
+  try {
+    const { name, email, content } = req.body;
+    const comment = new commentModel({ name, email, content, article: req.params.id });
+    await comment.save();
+    res.redirect(`/single/${req.params.id}`);
+  } catch (error) {
+    return next(createError('Error adding comment', 500));
+  }
 }
 
 module.exports = {
-    index,
-    articleByCategories,
-    singleArticle,
-    search,
-    author,
-    addComment
+  index,
+  articleByCategories,
+  singleArticle,
+  search,
+  author,
+  addComment
 }
--- routes/frontend.js
+++ routes/frontend.js
@@ -1,14 +1,34 @@
-const  express=require('express')
-const router=express.Router();
+const express = require('express');
+const router = express.Router();
+
 const siteController = require('../controllers/siteController');
+const loadCommonData = require('../middlewares/loadCommonData');
 
+router.use(loadCommonData);
 
-router.get('/',siteController.index);
-router.get('/category/:name',siteController.articleByCategories);
-router.get('/single/:id',siteController.singleArticle);
-router.get('/search',siteController.search);
-router.get('/author/:name',siteController.author);
-router.post('/single/:id',siteController.addComment);
+router.get('/', siteController.index);
+router.get('/category/:name', siteController.articleByCategories);
+router.get('/single/:id', siteController.singleArticle);
+router.get('/search', siteController.search);
+router.get('/author/:name', siteController.author);
+router.post('/single/:id/comment', siteController.addComment);
 
+// 404 Middleware
+router.use((req, res, next) => { 
+  res.status(404).render('404',{
+    message: 'Page not found'
+  })
+});
 
-module.exports=router;
+// 500 Error Handler
+router.use((err, req, res, next) => { 
+  console.error(err.stack);
+  const status = err.status || 500;
+  
+  res.status(status).render('errors',{
+    message: err.message || 'Something went wrong',
+    status
+  })
+});
+
+module.exports = router;
